7ed9831a3d133b4447810b5dcf4ad423816124f3
ShazeRx <perkowskikrzysio@gmail.com>
2021-08-16 19:56:39 +0200

    Added env management functionality

--- app.py
+++ app.py
@@ -3,12 +3,13 @@ import logging
 
 import websockets
 
+import settings
 from server.server import handler
 
 if __name__ == '__main__':
     # Run websocket server
     logging.basicConfig(level=logging.DEBUG)
-    start_server = websockets.serve(handler, "localhost", 5000)
+    start_server = websockets.serve(handler, settings.SERVER_URL, settings.SERVER_PORT)
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
     logging.info("Server started")
